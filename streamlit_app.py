c9cdef0ad4920da03930ec64577c3e2062b42f07
Piyush Ravi <rpiyush89@gmail.com>
2024-09-30 11:52:07 +0530

    Update streamlit_app.py
    fruityvice integration

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,10 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
+
+
+
+#fruityvice nutrition info
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
